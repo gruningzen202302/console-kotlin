7761ee7cc5a4c58adee474b81c8b073fdc7c3e5e
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-09-02 20:48:56 -0300

    feat:add link to the video for the sample data

--- matrix-string.kt
+++ matrix-string.kt
@@ -1,9 +1,16 @@
 fun main(){ 
-	println	("Prints a formatted matrix of 3 rows and 3 columns")
-	val row001 = "1 \t 2 \t 3 \n"
-	val row002 = "4 \t 5 \t 6 \n"
-	val row003 = "7 \t 8 \t 9 \n"
-	val matrix3by3 = row001 + row002 + row003
-	println(matrix3by3)
+	val youtubeVideLink = "https://youtu.be/cY4HiiFHO1o"
+	println(""" 
+Prints a formatted matrix of 4 rows and 4 columns
+using the values from this example of TSP
+link to the video $youtubeVideLink
+""")
+	val row001 = "0 \t 4 \t 1 \t 9  \n"
+	val row002 = "3 \t 0 \t 6 \t 11 \n"
+	val row003 = "4 \t 1 \t 0 \t 2  \n"
+	val row004 = "6 \t 5 \t -4\t 0  \n"
+	
+	val matrix4by4 = row001 + row002 + row003 + row004
+	println(matrix4by4)
 }
 
